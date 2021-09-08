ec0b172a0e32f19c19ae80481fed3083052c6b49
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-08 22:05:21 +0900

    chore: init my mod

---
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreClientMod.java
@@ -0,0 +1,12 @@
+package atonkish.reinfcore;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ReinforcedCoreClientMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreMod.java
@@ -0,0 +1,15 @@
+package atonkish.reinfcore;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReinforcedCoreMod implements ModInitializer {
+	public static final String MOD_ID = "reinfcore";
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
