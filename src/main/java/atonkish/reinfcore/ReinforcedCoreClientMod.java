a041bee76ec4fefdaf42c3c86d2512016d09486c
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-09-08 22:36:33 +0900

    feat: register item group and screens

--- src/main/java/atonkish/reinfcore/ReinforcedCoreClientMod.java
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreClientMod.java
@@ -3,10 +3,38 @@ package atonkish.reinfcore;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
+import net.minecraft.screen.ScreenHandlerType;
+
+import atonkish.reinfcore.client.gui.screen.ingame.ReinforcedStorageScreen;
+import atonkish.reinfcore.screen.ModScreenHandlerType;
+import atonkish.reinfcore.screen.ReinforcedStorageScreenHandler;
 
 @Environment(EnvType.CLIENT)
 public class ReinforcedCoreClientMod implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		registerScreen();
 	}
+
+	private static void registerScreen() {
+		// Single Block Screen
+		for (ScreenHandlerType<ReinforcedStorageScreenHandler> screenHandlerType : ModScreenHandlerType.REINFORCED_SINGLE_BLOCK_MAP
+				.values()) {
+			ScreenRegistry.register(screenHandlerType, ReinforcedStorageScreen::new);
+		}
+
+		// Double Block Screen
+		for (ScreenHandlerType<ReinforcedStorageScreenHandler> screenHandlerType : ModScreenHandlerType.REINFORCED_DOUBLE_BLOCK_MAP
+				.values()) {
+			ScreenRegistry.register(screenHandlerType, ReinforcedStorageScreen::new);
+		}
+
+		// Shulker Box Screen
+		for (ScreenHandlerType<ReinforcedStorageScreenHandler> screenHandlerType : ModScreenHandlerType.REINFORCED_SHULKER_BOX_MAP
+				.values()) {
+			ScreenRegistry.register(screenHandlerType, ReinforcedStorageScreen::new);
+		}
+	}
+
 }
