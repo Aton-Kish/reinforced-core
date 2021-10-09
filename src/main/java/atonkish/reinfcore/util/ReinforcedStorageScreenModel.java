74f3c8944de78f9068d0c0dcadd6c8961f4a8778
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-10-09 13:03:47 +0900

    feat: add inv size/cols calc method

--- src/main/java/atonkish/reinfcore/util/ReinforcedStorageScreenModel.java
+++ src/main/java/atonkish/reinfcore/util/ReinforcedStorageScreenModel.java
@@ -37,8 +37,8 @@ public enum ReinforcedStorageScreenModel {
     }
 
     private static Point2i calcPlayerInventoryPoint(ReinforcingMaterial material, boolean isDoubleBlock) {
-        int size = isDoubleBlock ? material.getSize() * 2 : material.getSize();
-        int cols = size <= 81 ? 9 : size / 9;
+        int size = calcContainerInventorySize(material, isDoubleBlock);
+        int cols = calcContainerInventoryColumns(size);
         int rows = size / cols;
 
         int x = 7 + (cols - 9) * 18 / 2;
@@ -71,5 +71,13 @@ public enum ReinforcedStorageScreenModel {
         }
 
         return (ReinforcedStorageScreenModel) null;
+    }
+
+    public static int calcContainerInventorySize(ReinforcingMaterial material, boolean isDoubleBlock) {
+        return isDoubleBlock ? material.getSize() * 2 : material.getSize();
+    }
+
+    public static int calcContainerInventoryColumns(int size) {
+        return size <= 81 ? 9 : size / 9;
     }
 }
