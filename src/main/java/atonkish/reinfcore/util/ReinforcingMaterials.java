d0b846473493352d98c346ac9767655ee0e8feed
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-01-22 23:37:37 +0900

    refactor: refactor args

--- src/main/java/atonkish/reinfcore/util/ReinforcingMaterials.java
+++ src/main/java/atonkish/reinfcore/util/ReinforcingMaterials.java
@@ -2,15 +2,12 @@ package atonkish.reinfcore.util;
 
 import java.util.LinkedHashMap;
 
-import net.minecraft.item.Item;
-
 public class ReinforcingMaterials {
     public static final LinkedHashMap<String, ReinforcingMaterial> MAP = new LinkedHashMap<>();
 
-    public static ReinforcingMaterial register(String name, int size, Item ingredient) {
-        ReinforcingMaterial material = new ReinforcingMaterial(name, size, ingredient);
-        if (!MAP.containsKey(name)) {
-            MAP.put(name, material);
+    public static ReinforcingMaterial register(ReinforcingMaterial material) {
+        if (!MAP.containsKey(material.getName())) {
+            MAP.put(material.getName(), material);
         }
 
         return material;
