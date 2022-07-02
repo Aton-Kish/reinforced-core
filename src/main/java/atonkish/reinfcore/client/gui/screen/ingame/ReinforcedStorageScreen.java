f41feb0172a13aa02a44cb0f1476ef607000cc9c
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-07-02 13:21:58 +0900

    feat: vanilla screen texture

--- src/main/java/atonkish/reinfcore/client/gui/screen/ingame/ReinforcedStorageScreen.java
+++ src/main/java/atonkish/reinfcore/client/gui/screen/ingame/ReinforcedStorageScreen.java
@@ -11,7 +11,6 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
-import atonkish.reinfcore.ReinforcedCoreMod;
 import atonkish.reinfcore.screen.ReinforcedStorageScreenHandler;
 import atonkish.reinfcore.util.ReinforcedStorageScreenModel;
 import atonkish.reinfcore.util.ReinforcedStorageScreenModels;
@@ -19,8 +18,7 @@ import atonkish.reinfcore.util.math.Point2i;
 
 @Environment(EnvType.CLIENT)
 public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScreenHandler> {
-    private static final Identifier TEXTURE = new Identifier(ReinforcedCoreMod.MOD_ID,
-            "textures/gui/container/generic.png");
+    private static final Identifier TEXTURE = new Identifier("textures/gui/container/generic_54.png");
     private final ReinforcedStorageScreenModel screenModel;
     private final int cols;
     private final int rows;
@@ -63,11 +61,11 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
         Point2i containerInventoryPoint = this.screenModel.getContainerInventoryPoint();
         Point2i playerInventoryPoint = this.screenModel.getPlayerInventoryPoint();
         int horizontalDrawCount;
-        int horizontalDrawNum = this.cols / 14;
-        int horizontalDrawFraction = (this.cols % 14) * 18;
+        int horizontalDrawNum = this.cols / 9;
+        int horizontalDrawFraction = (this.cols % 9) * 18;
         int verticalDrawCount;
-        int verticalDrawNum = this.rows / 3;
-        int verticalDrawFraction = (this.rows % 3) * 18;
+        int verticalDrawNum = this.rows / 6;
+        int verticalDrawFraction = (this.rows % 6) * 18;
         int offsetX = 0;
         int offsetY = 0;
 
@@ -77,21 +75,21 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
         for (verticalDrawCount = 0; verticalDrawCount < verticalDrawNum; ++verticalDrawCount) {
             offsetX = containerInventoryPoint.getX();
             for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-                this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, 14 * 18, 3 * 18);
-                offsetX += 14 * 18;
+                this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 7, 17, 9 * 18, 6 * 18);
+                offsetX += 9 * 18;
             }
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, horizontalDrawFraction, 3 * 18);
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 7, 17, horizontalDrawFraction, 6 * 18);
             offsetX += horizontalDrawFraction;
 
-            offsetY += 3 * 18;
+            offsetY += 6 * 18;
         }
 
         offsetX = containerInventoryPoint.getX();
         for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, 14 * 18, verticalDrawFraction);
-            offsetX += 14 * 18;
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 7, 17, 9 * 18, verticalDrawFraction);
+            offsetX += 9 * 18;
         }
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, horizontalDrawFraction,
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 7, 17, horizontalDrawFraction,
                 verticalDrawFraction);
         offsetX += horizontalDrawFraction;
 
@@ -100,18 +98,16 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
         // Player Inventory Slots
         offsetX = playerInventoryPoint.getX();
         offsetY = playerInventoryPoint.getY();
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, 9 * 18, 3 * 18);
-        offsetY += 3 * 18 + 4;
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 198, 9 * 18, 1 * 18);
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 7, 139, 9 * 18, 3 * 18 + 4 + 1 * 18);
     }
 
     private void drawBackgroundTexture(MatrixStack matrices) {
         int horizontalDrawCount;
-        int horizontalDrawNum = (this.backgroundWidth - 8) / 244;
-        int horizontalDrawFraction = (this.backgroundWidth - 8) % 244;
+        int horizontalDrawNum = (this.backgroundWidth - 8) / (176 - 8);
+        int horizontalDrawFraction = (this.backgroundWidth - 8) % (176 - 8);
         int verticalDrawCount;
-        int verticalDrawNum = (this.backgroundHeight - 8) / 190;
-        int verticalDrawFraction = (this.backgroundHeight - 8) % 190;
+        int verticalDrawNum = (this.backgroundHeight - 8) / (222 - 8);
+        int verticalDrawFraction = (this.backgroundHeight - 8) % (222 - 8);
         int offsetX = 0;
         int offsetY = 0;
 
@@ -125,13 +121,13 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
         offsetX += 4;
         // left-right
         for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 0, 244, 4);
-            offsetX += 244;
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 0, 176 - 8, 4);
+            offsetX += 176 - 8;
         }
         this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 0, horizontalDrawFraction, 4);
         offsetX += horizontalDrawFraction;
         // right
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 248, 0, 4, 4);
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 176 - 4, 0, 4, 4);
         offsetX += 4;
 
         offsetY += 4;
@@ -143,38 +139,51 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
         for (verticalDrawCount = 0; verticalDrawCount < verticalDrawNum; ++verticalDrawCount) {
             offsetX = 0;
             // left
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 4, 4, 190);
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 4, 4, 222 - 8);
             offsetX += 4;
-            // left-right
-            for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-                this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, 244, 190);
-                offsetX += 244;
-            }
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, horizontalDrawFraction, 190);
-            offsetX += horizontalDrawFraction;
+            offsetX += this.backgroundWidth - 8;
             // right
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 248, 4, 4, 190);
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 176 - 4, 4, 4, 222 - 8);
             offsetX += 4;
 
-            offsetY += 190;
+            offsetY += 222 - 8;
         }
 
         offsetX = 0;
         // left
         this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 4, 4, verticalDrawFraction);
         offsetX += 4;
-        // left-right
+        offsetX += this.backgroundWidth - 8;
+        // right
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 176 - 4, 4, 4, verticalDrawFraction);
+        offsetX += 4;
+
+        offsetY += verticalDrawFraction;
+
+        // center
+        offsetY = 4;
+        verticalDrawNum = (this.backgroundHeight - 8) / 13;
+        verticalDrawFraction = (this.backgroundHeight - 8) % 13;
+        for (verticalDrawCount = 0; verticalDrawCount < verticalDrawNum; ++verticalDrawCount) {
+            offsetX = 4;
+            for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
+                this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, 176 - 8, 13);
+                offsetX += 176 - 8;
+            }
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, horizontalDrawFraction, 13);
+            offsetX += horizontalDrawFraction;
+
+            offsetY += 13;
+        }
+
+        offsetX = 4;
         for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, 244, verticalDrawFraction);
-            offsetX += 244;
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, 176 - 8, verticalDrawFraction);
+            offsetX += 176 - 8;
         }
         this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 4, horizontalDrawFraction,
                 verticalDrawFraction);
         offsetX += horizontalDrawFraction;
-        // right
-
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 248, 4, 4, verticalDrawFraction);
-        offsetX += 4;
 
         offsetY += verticalDrawFraction;
 
@@ -184,17 +193,17 @@ public class ReinforcedStorageScreen extends HandledScreen<ReinforcedStorageScre
 
         offsetX = 0;
         // left
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 194, 4, 4);
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 0, 222 - 4, 4, 4);
         offsetX += 4;
         // left-right
         for (horizontalDrawCount = 0; horizontalDrawCount < horizontalDrawNum; ++horizontalDrawCount) {
-            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 194, 244, 4);
-            offsetX += 244;
+            this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 222 - 4, 176 - 8, 4);
+            offsetX += 176 - 8;
         }
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 194, horizontalDrawFraction, 4);
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 4, 222 - 4, horizontalDrawFraction, 4);
         offsetX += horizontalDrawFraction;
         // right
-        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 248, 194, 4, 4);
+        this.drawTexture(matrices, this.x + offsetX, this.y + offsetY, 176 - 4, 222 - 4, 4, 4);
         offsetX += 4;
 
         offsetY += 4;
