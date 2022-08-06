85c5fa1e35df4be06fdb19a19729047ef869a5ab
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-07 00:25:50 +0900

    feat: add mod config

---
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreConfig.java
@@ -0,0 +1,9 @@
+package atonkish.reinfcore;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = ReinforcedCoreMod.MOD_ID)
+public class ReinforcedCoreConfig implements ConfigData {
+    public boolean scrollScreen = false;
+}
