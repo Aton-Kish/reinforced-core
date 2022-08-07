25741cef510bbd0e8f2677f565fe6cf69940ab16
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-07 17:53:56 +0900

    fix: fix screen model

--- src/main/java/atonkish/reinfcore/util/ReinforcedStorageScreenModel.java
+++ src/main/java/atonkish/reinfcore/util/ReinforcedStorageScreenModel.java
@@ -16,38 +16,10 @@ public class ReinforcedStorageScreenModel {
 
     private final ReinforcingMaterial material;
     private final boolean isDoubleBlock;
-    private final Point2i containerInventoryPoint;
-    private final Point2i playerInventoryPoint;
-
-    public ReinforcedStorageScreenModel(ReinforcingMaterial material, boolean isDoubleBlock,
-            Point2i containerInventoryPoint, Point2i playerInventoryPoint) {
-        this.material = material;
-        this.isDoubleBlock = isDoubleBlock;
-        this.containerInventoryPoint = containerInventoryPoint;
-        this.playerInventoryPoint = playerInventoryPoint;
-    }
 
     public ReinforcedStorageScreenModel(ReinforcingMaterial material, boolean isDoubleBlock) {
-        this(material, isDoubleBlock, calcContainerInventoryPoint(material, isDoubleBlock),
-                calcPlayerInventoryPoint(material, isDoubleBlock));
-    }
-
-    private static Point2i calcContainerInventoryPoint(ReinforcingMaterial material, boolean isDoubleBlock) {
-        int x = CONTAINER_INVENTORY_X;
-        int y = CONTAINER_INVENTORY_Y;
-
-        return new Point2i(x, y);
-    }
-
-    private static Point2i calcPlayerInventoryPoint(ReinforcingMaterial material, boolean isDoubleBlock) {
-        int size = getContainerInventorySize(material, isDoubleBlock);
-        int cols = getContainerInventoryColumns(size);
-        int rows = getContainerInventoryRows(size, cols);
-
-        int x = CONTAINER_INVENTORY_X + (cols - SINGLE_SCREEN_DEFAULT_COLS) * SLOT_SIZE / 2;
-        int y = CONTAINER_INVENTORY_Y + rows * SLOT_SIZE + GAP_BETWEEN_CONTAINER_INVENTORY_AND_PLAYER_INVENTORY;
-
-        return new Point2i(x, y);
+        this.material = material;
+        this.isDoubleBlock = isDoubleBlock;
     }
 
     public ReinforcingMaterial getMaterial() {
@@ -59,11 +31,21 @@ public class ReinforcedStorageScreenModel {
     }
 
     public Point2i getContainerInventoryPoint() {
-        return this.containerInventoryPoint;
+        int x = CONTAINER_INVENTORY_X;
+        int y = CONTAINER_INVENTORY_Y;
+
+        return new Point2i(x, y);
     }
 
     public Point2i getPlayerInventoryPoint() {
-        return this.playerInventoryPoint;
+        int size = getContainerInventorySize(this.material, this.isDoubleBlock);
+        int cols = getContainerInventoryColumns(size);
+        int rows = getContainerInventoryRows(size, cols);
+
+        int x = CONTAINER_INVENTORY_X + (cols - SINGLE_SCREEN_DEFAULT_COLS) * SLOT_SIZE / 2;
+        int y = CONTAINER_INVENTORY_Y + rows * SLOT_SIZE + GAP_BETWEEN_CONTAINER_INVENTORY_AND_PLAYER_INVENTORY;
+
+        return new Point2i(x, y);
     }
 
     public static int getContainerInventorySize(ReinforcingMaterial material, boolean isDoubleBlock) {
