d5be43f0f38cb1e558c605dbcd9a985b5030f5f1
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-07 22:30:11 +0900

    bump version from 2.1.1+1.19 to 3.0.0+1.19  (#18)
    * chore: bump deps versions
    
    * chore: add modmenu deps
    
    * feat: add mod config
    
    * feat: modmenu integ
    
    * feat: refactor screen type selector
    
    * feat: add slot accessor
    
    * feat: refactor gui screen
    
    * fix: fix screen model
    
    * fix: fix scroll condition
    
    * feat: add scroll feature
    
    * fix: fix outside position
    
    * feat: add @Override
    
    * feat: refactor screen type string
    
    * feat: refactor item group mixin
    
    * chore: bump deps version
    
    * chore: remove unused texture
    
    * feat: add scroll screen config
    
    * fix: fix variable name
    
    * chore: bump version from 2.1.1+1.19 to 3.0.0+1.19
    
    * chore: format
    
    * chore: add build trigger

---
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreConfig.java
@@ -0,0 +1,21 @@
+package atonkish.reinfcore;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+
+import atonkish.reinfcore.util.ReinforcedStorageScreenType;
+
+@Config(name = ReinforcedCoreMod.MOD_ID)
+public class ReinforcedCoreConfig implements ConfigData {
+    @ConfigEntry.Gui.EnumHandler(option = ConfigEntry.Gui.EnumHandler.EnumDisplayOption.BUTTON)
+    public ReinforcedStorageScreenType screenType = ReinforcedStorageScreenType.SINGLE;
+
+    @ConfigEntry.Gui.CollapsibleObject
+    public ScrollScreen scrollScreen = new ScrollScreen();
+
+    public class ScrollScreen {
+        @ConfigEntry.BoundedDiscrete(min = 6, max = 9)
+        public int rows = 6;
+    }
+}
---
+++ src/main/java/atonkish/reinfcore/mixin/SlotAccessor.java
@@ -0,0 +1,18 @@
+package atonkish.reinfcore.mixin;
+
+import net.minecraft.screen.slot.Slot;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Slot.class)
+public interface SlotAccessor {
+    @Mutable
+    @Accessor("x")
+    public void setX(int x);
+
+    @Mutable
+    @Accessor("y")
+    public void setY(int y);
+}
