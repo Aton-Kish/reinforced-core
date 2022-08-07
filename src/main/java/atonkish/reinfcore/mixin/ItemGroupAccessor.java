f09c37221c4382775b715b78e7f9e43db2beeb4e
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-07 22:22:05 +0900

    chore: format

--- src/main/java/atonkish/reinfcore/mixin/ItemGroupAccessor.java
+++ src/main/java/atonkish/reinfcore/mixin/ItemGroupAccessor.java
@@ -7,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-
 @Mixin(ItemGroup.class)
 public interface ItemGroupAccessor {
     @Mutable
--- src/main/java/atonkish/reinfcore/mixin/SlotAccessor.java
+++ src/main/java/atonkish/reinfcore/mixin/SlotAccessor.java
@@ -6,7 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-
 @Mixin(Slot.class)
 public interface SlotAccessor {
     @Mutable
