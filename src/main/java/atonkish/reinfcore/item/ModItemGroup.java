eaa56c07fcddcd580eceb0d0bcdbbdba836bc4af
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2022-08-07 20:45:26 +0900

    feat: refactor item group mixin

--- src/main/java/atonkish/reinfcore/item/ModItemGroup.java
+++ src/main/java/atonkish/reinfcore/item/ModItemGroup.java
@@ -1,15 +1,23 @@
 package atonkish.reinfcore.item;
 
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 import atonkish.reinfcore.ReinforcedCoreMod;
+import atonkish.reinfcore.mixin.ItemGroupAccessor;
 
 public class ModItemGroup {
     public static final ItemGroup REINFORCED_STORAGE;
 
     public static void init() {
+    }
+
+    public static void setIcon(ItemGroup itemGroup, ItemConvertible item) {
+        ItemStack itemStack = new ItemStack(item);
+        ((ItemGroupAccessor) itemGroup).setIcon(itemStack);
     }
 
     private static ItemGroup create(String id) {
