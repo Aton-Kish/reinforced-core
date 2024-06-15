f09c36524d8f6c5a3417f1f87f5c405aec0fc273
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-06-15 13:20:31 +0900

    added reinfcore-gametest entrypoint

--- src/main/java/atonkish/reinfcore/ReinforcedCoreMod.java
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreMod.java
@@ -1,13 +1,20 @@
 package atonkish.reinfcore;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+
+import net.minecraft.test.TestFunctions;
 
 import atonkish.reinfcore.api.ReinforcedCoreModInitializer;
 import atonkish.reinfcore.item.ModItemGroups;
@@ -16,6 +23,7 @@ public class ReinforcedCoreMod implements ModInitializer {
 	public static final String MOD_ID = "reinfcore";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static ReinforcedCoreConfig CONFIG;
+	private static final Map<Class<?>, String> GAME_TEST_IDS = new HashMap<>();
 
 	@Override
 	public void onInitialize() {
@@ -30,5 +38,28 @@ public class ReinforcedCoreMod implements ModInitializer {
 		FabricLoader.getInstance()
 				.getEntrypoints(MOD_ID, ReinforcedCoreModInitializer.class)
 				.forEach(ReinforcedCoreModInitializer::onInitializeReinforcedCore);
+
+		// entrypoint: "reinfcore-gametest"
+		ReinforcedCoreMod.initializeReinforcedCoreGameTest();
+	}
+
+	private static void initializeReinforcedCoreGameTest() {
+		List<EntrypointContainer<Object>> entrypointContainers = FabricLoader.getInstance()
+				.getEntrypointContainers(String.format("%s-gametest", MOD_ID), Object.class);
+
+		for (EntrypointContainer<Object> container : entrypointContainers) {
+			Class<?> testClass = container.getEntrypoint().getClass();
+			String modid = container.getProvider().getMetadata().getId();
+
+			if (GAME_TEST_IDS.containsKey(testClass)) {
+				throw new UnsupportedOperationException("Test class (%s) has already been registered with mod (%s)"
+						.formatted(testClass.getCanonicalName(), modid));
+			}
+
+			GAME_TEST_IDS.put(testClass, modid);
+			TestFunctions.register(testClass);
+
+			LOGGER.debug("Registered test class {} for mod {}", testClass.getCanonicalName(), modid);
+		}
 	}
 }
