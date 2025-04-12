45919ee5df044414b576f01ed28fda0eb74de7e7
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2025-04-13 00:33:57 +0900

    [v4.0.5+1.21.5] support Minecraft 1.21.5 & fixed Aton-Kish/reinforced-shulker-boxes#89 (#45)
    * bump deps
    
    * remove gametest
    
    * fixed https://github.com/Aton-Kish/reinforced-shulker-boxes/issues/89
    
    * re-added gametest entrypoint
    
    * remove a log
    
    * bump up lib version to v4.0.5+1.21.5

--- src/main/java/atonkish/reinfcore/ReinforcedCoreMod.java
+++ src/main/java/atonkish/reinfcore/ReinforcedCoreMod.java
@@ -1,29 +1,38 @@
 package atonkish.reinfcore;
 
-import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.minecraft.test.TestFunctions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryLoader;
+import net.minecraft.test.TestEnvironmentDefinition;
+import net.minecraft.test.TestInstance;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.impl.gametest.FabricGameTestRunner;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 import atonkish.reinfcore.api.ReinforcedCoreModInitializer;
+import atonkish.reinfcore.gametest.TestAnnotationLocator;
+import atonkish.reinfcore.gametest.TestFunction;
 import atonkish.reinfcore.item.ModItemGroups;
 
 public class ReinforcedCoreMod implements ModInitializer {
 	public static final String MOD_ID = "reinfcore";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static ReinforcedCoreConfig CONFIG;
-	private static final Map<Class<?>, String> GAME_TEST_IDS = new HashMap<>();
+	private static TestAnnotationLocator locator = new TestAnnotationLocator(FabricLoader.getInstance());
 
 	@Override
 	public void onInitialize() {
@@ -40,26 +49,34 @@ public class ReinforcedCoreMod implements ModInitializer {
 				.forEach(ReinforcedCoreModInitializer::onInitializeReinforcedCore);
 
 		// entrypoint: "reinfcore-gametest"
-		ReinforcedCoreMod.initializeReinforcedCoreGameTest();
+		this.onInitializeReinforcedCoreGameTest();
 	}
 
-	private static void initializeReinforcedCoreGameTest() {
-		List<EntrypointContainer<Object>> entrypointContainers = FabricLoader.getInstance()
-				.getEntrypointContainers(String.format("%s-gametest", MOD_ID), Object.class);
+	private void onInitializeReinforcedCoreGameTest() {
+		if (!(FabricGameTestRunner.ENABLED || FabricLoader.getInstance().isDevelopmentEnvironment())) {
+			return;
+		}
 
-		for (EntrypointContainer<Object> container : entrypointContainers) {
-			Class<?> testClass = container.getEntrypoint().getClass();
-			String modid = container.getProvider().getMetadata().getId();
+		for (TestFunction testFunction : locator.getTestFunctions()) {
+			LOGGER.debug("Registering test function: {}", testFunction.identifier());
+			Registry.register(Registries.TEST_FUNCTION, testFunction.identifier(), testFunction.testFunction());
+		}
+	}
 
-			if (GAME_TEST_IDS.containsKey(testClass)) {
-				throw new UnsupportedOperationException("Test class (%s) has already been registered with mod (%s)"
-						.formatted(testClass.getCanonicalName(), modid));
-			}
+	public static void registerDynamicEntries(List<RegistryLoader.Loader<?>> registriesList) {
+		Map<RegistryKey<? extends Registry<?>>, Registry<?>> registries = new IdentityHashMap<>(registriesList.size());
 
-			GAME_TEST_IDS.put(testClass, modid);
-			TestFunctions.register(testClass);
+		for (RegistryLoader.Loader<?> entry : registriesList) {
+			registries.put(entry.registry().getKey(), entry.registry());
+		}
 
-			LOGGER.debug("Registered test class {} for mod {}", testClass.getCanonicalName(), modid);
+		Registry<TestInstance> testInstances = (Registry<TestInstance>) registries.get(RegistryKeys.TEST_INSTANCE);
+		Registry<TestEnvironmentDefinition> testEnvironmentDefinitionRegistry = (Registry<TestEnvironmentDefinition>) Objects
+				.requireNonNull(registries.get(RegistryKeys.TEST_ENVIRONMENT));
+
+		for (TestFunction testFunction : locator.getTestFunctions()) {
+			TestInstance testInstance = testFunction.testInstance(testEnvironmentDefinitionRegistry);
+			Registry.register(testInstances, testFunction.identifier(), testInstance);
 		}
 	}
 }
---
+++ src/main/java/atonkish/reinfcore/gametest/TestAnnotationLocator.java
@@ -0,0 +1,59 @@
+package atonkish.reinfcore.gametest;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.entrypoint.EntrypointContainer;
+
+import atonkish.reinfcore.ReinforcedCoreMod;
+
+public final class TestAnnotationLocator {
+    private static final String ENTRYPOINT_KEY = String.format("%s-gametest", ReinforcedCoreMod.MOD_ID);
+
+    private final FabricLoader fabricLoader;
+
+    private List<TestFunction> testFunctions = null;
+
+    public TestAnnotationLocator(FabricLoader fabricLoader) {
+        this.fabricLoader = fabricLoader;
+    }
+
+    public List<TestFunction> getTestFunctions() {
+        if (testFunctions != null) {
+            return testFunctions;
+        }
+
+        List<EntrypointContainer<Object>> entrypointContainers = fabricLoader
+                .getEntrypointContainers(ENTRYPOINT_KEY, Object.class);
+
+        return testFunctions = entrypointContainers.stream()
+                .flatMap(entrypoint -> findMagicMethods(entrypoint).stream())
+                .toList();
+    }
+
+    private List<TestFunction> findMagicMethods(EntrypointContainer<Object> entrypoint) {
+        Class<?> testClass = entrypoint.getEntrypoint().getClass();
+        List<TestFunction> functions = new ArrayList<>();
+        for (Method method : testClass.getDeclaredMethods()) {
+            CustomTestProvider customTestProvider = method.getAnnotation(CustomTestProvider.class);
+            if (customTestProvider != null) {
+                functions.addAll(invokeCustomTestProviderMethod(method));
+            }
+        }
+
+        return functions;
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Collection<TestFunction> invokeCustomTestProviderMethod(Method method) {
+        try {
+            Object object = method.getDeclaringClass().getDeclaredConstructor().newInstance();
+            return (Collection<TestFunction>) method.invoke(object);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
